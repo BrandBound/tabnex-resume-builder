9864ee4e232ca2cfb49c2d5fab3dc8b7969f05b4
Mohamed <mmmoussa@uwaterloo.ca>
2015-11-18 23:46:24 -0500

    Created initial input form for user details
    User (not authenticated) can enter details and they are then saved to a
    Mongo object

--- tabnex-resume-builder/tabnex-resume-builder.js
+++ tabnex-resume-builder/tabnex-resume-builder.js
@@ -1,23 +1,49 @@
+Users = new Mongo.Collection("users");
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+	// Meteor.subscribe("users");
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+	Template.personalDetailsForm.helpers({
+		
+	});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
+	Template.personalDetailsForm.events({
+		'submit form': function (event) {
+			event.preventDefault();
+			var firstName = event.target.firstName.value;
+			var lastName = event.target.lastName.value;
+			var fullName = firstName + " " + lastName;
+			var email = event.target.email.value;
+			var phone = event.target.phone.value;
+			var summary = event.target.summary.value;
+			var passObj = {
+				"name": fullName,
+				"email": email,
+				"phone": phone,
+				"summary": summary
+			}
+			console.log(fullName);
+			Meteor.call("addUser", passObj);
+			console.log("ay");
+		}
+	});
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+	Meteor.startup(function () {
+		// code to run on server at startup
+
+	});
+
+	// Meteor.publish("users", function () {
+	// 	return Users.find();
+	// });
 }
+
+Meteor.methods({
+	addUser: function(obj) {
+		Users.remove({});
+		console.log(obj);	
+		Users.insert(obj);
+	}
+});
